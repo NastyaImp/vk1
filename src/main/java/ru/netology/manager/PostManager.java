4adc75c9c161c68177667f1f47c15f96d95784b2
NastyaImp <Imperiian@yandex.ru>
2021-12-03 20:02:20 +0300

    manager add

---
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -0,0 +1,15 @@
+package ru.netology.manager;
+
+import ru.netology.domain.Post;
+
+public class PostManager {
+    private Post[] post;
+
+    public Post[] PostSearch(int idOwner, int idPost, int date, String query, int count) {
+        return post;
+    }
+
+    public void delete(int idOwner, int idPost) {
+
+    }
+}
